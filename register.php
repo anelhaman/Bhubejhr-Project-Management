d3a9610951e5c63319a39378a5191ac1bc043e0f
mrjimmy18@gmail.com <mrjimmy18@gmail.com>
2017-12-01 18:58:09 +0700

    Register Page Redesign

--- register.php
+++ register.php
@@ -1,6 +1,5 @@
 <?php
 require_once 'autoload.php';
-
 ?>
 
 <!DOCTYPE html>
@@ -14,74 +13,36 @@ require_once 'autoload.php';
 <meta name="viewport" content="user-scalable=no">
 <meta name="viewport" content="initial-scale=1,maximum-scale=1">
 
-<title><?php echo TITLE;?></title>
-
+<title>ลงทะเบียน</title>
 <link rel="stylesheet" type="text/css" href="css/style.css"/>
 <link rel="stylesheet" type="text/css" href="plugin/font-awesome/css/font-awesome.min.css"/>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body>
-
-<?php include_once'header.php';?>
-
-
-	<div class="container">
-			<form class="form-horizontal" action="added.php" method="POST">
-				<fieldset>
-
-				<!-- Form Name -->
-				<legend>ลงทะเบียน</legend>
-
-				<!-- Text input-->
-				<div class="form-group">
-				  <label class="col-md-4 control-label" for="tel">เบอร์โทร</label>
-				  <div class="col-md-5">
-				  <input id="tel" name="tel" type="number" maxlength="10" placeholder="กรอกเบอร์โทร" class="form-control input-md" required="" autofocus>
-
-				  </div>
-				</div>
-
-				<!-- Password input-->
-				<div class="form-group">
-				  <label class="col-md-4 control-label" for="password">รหัสผ่าน</label>
-				  <div class="col-md-5">
-				    <input id="password" name="password" type="password" placeholder="ตัวเลข 4 ตัว" class="form-control input-md" required="" maxlength="4">
-
-				  </div>
-				</div>
-
-				<!-- Text input-->
-				<div class="form-group">
-				  <label class="col-md-4 control-label" for="name">ชื่อจริง</label>
-				  <div class="col-md-5">
-				  <input id="name" name="name" type="text" placeholder="กรอกชื่อจริง" class="form-control input-md" required="">
-
-				  </div>
-				</div>
-
-				<!-- Text input-->
-				<div class="form-group">
-				  <label class="col-md-4 control-label" for="lastname">นามสกุล</label>
-				  <div class="col-md-5">
-				  <input id="lastname" name="lastname" type="text" placeholder="กรอกนามสกุล" class="form-control input-md" required="">
-
-				  </div>
-				</div>
-
-				<!-- Button -->
-				<div class="form-group">
-				  <label class="col-md-4 control-label" for="submit"></label>
-				  <div class="col-md-4">
-				    <button id="submit" class="btn btn-info">ยืนยัน</button>
-				  </div>
-				</div>
-
-				</fieldset>
-				</form>
-
+<body class="registerBG">
+<div class="register">
+	<div class="head">
+		<div class="logo">
+			<img src="image/logo.png" alt="">
+		</div>
+		<div class="text">
+			<h1>Bhubejhr Project Management</h1>
+			<p>Chaophya Abhaibhubejhr Hospital</p>
+		</div>
 	</div>
 
+	<div class="form">
+		<label for="fullname">ชื่อ-นามสกุล</label>
+		<input type="text" id="fullname" placeholder="ชื่อ-นามสกุล" autofocus>
+		<label for="phone">เบอร์โทรศัพท์</label>
+		<input type="text" id="phone" placeholder="เบอร์โทรศัพท์">
+		<label for="info">ชื่อฝ่ายและแผนก</label>
+		<textarea id="info" placeholder="ชื่อฝ่ายและแผนก"></textarea>
+		<label for="password">รหัสผ่าน</label>
+		<input type="text" id="password" placeholder="รหัสผ่าน">
 
+		<button type="btn">ลงทะเบียน</button>
+	</div>
 
+	<div class="note">หากคุณเคยลงทะเบียน ไปที่ <a href="login.php">เข้าสู่ระบบ</a></div>
+</div>
 </body>
 </html>
